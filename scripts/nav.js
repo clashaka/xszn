732e329ce2df3281b6d994a28311fe799dba426a
clashaka <clashaka@outlook.com>
2024-09-22 17:15:28 +0800

    设置分类下的子类

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,40 +1,45 @@
-import link from "./link.js";
-
 export default () => {
-    // 遍历导航
-    const ul = document.querySelector('nav ul');
+    async function fetchLink() {
+        const response = await fetch('./data/link.json');
+        const link = await response.json();
 
-    for (const key in link) {
-        const li = document.createElement('li');
-        li.textContent = key;
-        ul.appendChild(li);
+        // 遍历导航
+        const ul = document.querySelector('nav ul');
+
+        for (const key in link) {
+            const li = document.createElement('li');
+            li.textContent = key;
+            ul.appendChild(li);
+        }
+
+        const li = document.querySelectorAll('nav li');
+        const container = document.querySelector('main .container')
+
+        // 选项卡列表高亮、追加选项卡面板
+        li.forEach((item, index) => {
+
+            item.addEventListener('click', () => {
+                li.forEach(item => item.style.color = 'darkgray');
+                item.style.color = 'var(--text-color)';
+            })
+
+            if (index > 0) {
+                const div = document.createElement('div');
+                div.classList.add('tabs')
+                container.appendChild(div);
+            }
+        })
+
+        // 选项卡面板显示
+        const tabs = document.querySelectorAll('main .tabs')
+
+        li.forEach((item, index) => {
+            item.addEventListener('click', () => {
+                tabs.forEach(item => item.style.display = 'none');
+                tabs[index].style.display = 'block';
+            })
+        })
     }
 
-    const li = document.querySelectorAll('nav li');
-    const container = document.querySelector('main .container')
-
-    // 选项卡列表高亮、追加选项卡面板
-    li.forEach((item, index) => {
-
-        item.addEventListener('click', () => {
-            li.forEach(item => item.style.color = 'darkgray');
-            item.style.color = 'var(--text-color)';
-        })
-
-        if (index > 0) {
-            const div = document.createElement('div');
-            div.classList.add('tabs')
-            container.appendChild(div);
-        }
-    })
-
-    // 选项卡面板显示
-    const tabs = document.querySelectorAll('main .tabs')
-
-    li.forEach((item, index) => {
-        item.addEventListener('click', () => {
-            tabs.forEach(item => item.style.display = 'none');
-            tabs[index].style.display = 'block';
-        })
-    })
+    fetchLink();
 }
--- scripts/tab.js
+++ scripts/tab.js
@@ -1,32 +1,36 @@
-import link from "./link.js";
-
 export default function tab() {
-    const tabs = document.querySelectorAll('.tabs');
-    const tabContent = document.querySelector('.tab-content');
-    const linkArr = [];
-    let recommend = [];
 
-    // 遍历 link 中的网址，将网址项保存到新数组，作为推荐项的内容展示
-    // 推荐项的内容展示
-    for (const key in link) {
-        const values = link[key];
-        for (const value of values) {
-            recommend.push(value);
+    async function fetchLink() {
+        const response = await fetch('./data/link.json');
+        const link = await response.json();
+
+
+        const tabs = document.querySelectorAll('.tabs');
+        const tabContent = document.querySelector('.tab-content');
+        const linkArr = [];
+        let recommend = [];
+
+        // 遍历 link 中的网址，将网址项保存到新数组，作为推荐项的内容展示
+        // 推荐项的内容展示
+        for (const key in link) {
+            const values = link[key];
+            for (const value of values) {
+                recommend.push(value);
+            }
         }
-    }
 
-    // 其余项的内容展示
-    for (const key in link) {
-        linkArr.push(key);
-    }
+        // 其余项的内容展示
+        for (const key in link) {
+            linkArr.push(key);
+        }
 
-    while (recommend.length > 0) {
-        const random = Math.floor(Math.random() * recommend.length);
+        while (recommend.length > 0) {
+            const random = Math.floor(Math.random() * recommend.length);
 
-        const a = document.createElement('a');
-        a.href = recommend[random].url;
-        a.target = '_blank';
-        a.innerHTML = `
+            const a = document.createElement('a');
+            a.href = recommend[random].url;
+            a.target = '_blank';
+            a.innerHTML = `
                     <div class="card">
                         <figure class="card-head">
                             <div class="card-icon">
@@ -42,32 +46,32 @@ export default function tab() {
                         </div>
                     </div>
                     `;
-        tabContent.appendChild(a);
-        recommend.splice(random, 1)
-    }
-
-    tabs.forEach((item, index) => {
-
-        // 根据 Nav 除推荐项外生成 Tabs Content Dom
-        if (index > 0) {
-            const tabContent = document.createElement('div');
-
-            tabContent.classList.add('tab-content');
-            item.appendChild(tabContent);
+            tabContent.appendChild(a);
+            recommend.splice(random, 1)
         }
 
-        const value = linkArr[index - 1];
-        const values = link[value];
+        tabs.forEach((item, index) => {
 
-        // is.Array 判断一个值是否是数组，返回布尔值
-        if (values && Array.isArray(values)) {
-            const tabContent = document.querySelectorAll('.tab-content');
+            // 根据 Nav 除推荐项外生成 Tabs Content Dom
+            if (index > 0) {
+                const tabContent = document.createElement('div');
 
-            values.forEach((item) => {
-                const a = document.createElement('a');
-                a.href = item.url;
-                a.target = '_blank';
-                a.innerHTML = `
+                tabContent.classList.add('tab-content');
+                item.appendChild(tabContent);
+            }
+
+            const value = linkArr[index - 1];
+            const values = link[value];
+
+            // is.Array 判断一个值是否是数组，返回布尔值
+            if (values && Array.isArray(values)) {
+                const tabContent = document.querySelectorAll('.tab-content');
+
+                values.forEach((item) => {
+                    const a = document.createElement('a');
+                    a.href = item.url;
+                    a.target = '_blank';
+                    a.innerHTML = `
                         <div class="card">
                             <figure class="card-head">
                                 <div class="card-icon">
@@ -83,15 +87,18 @@ export default function tab() {
                             </div>
                         </div>
                 `;
-                tabContent[index].appendChild(a);
-            });
-        }
-    })
+                    tabContent[index].appendChild(a);
+                });
+            }
+        })
 
-    // Tabs 各容器同高
-    const tabOne = document.querySelector('.tabs:nth-child(1)');
+        // Tabs 各容器同高
+        const tabOne = document.querySelector('.tabs:nth-child(1)');
 
-    tabs.forEach(item => {
-        item.style.height = `${tabOne.clientHeight}px`;
-    })
+        tabs.forEach(item => {
+            item.style.height = `${tabOne.clientHeight}px`;
+        })
+    }
+
+    fetchLink();
 }
