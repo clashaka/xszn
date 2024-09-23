b1a993227e99da4becb79ec31ebdcaea5559fa23
clashaka <clashaka@outlook.com>
2024-09-23 16:32:54 +0800

    Add new content

--- scripts/modules/theme.js
+++ scripts/modules/theme.js
@@ -3,25 +3,27 @@ function theme() {
     const light = document.querySelector('#light');
     const dark = document.querySelector('#dark');
 
-    // 持久化主题切换
+    // 持久化主题
     if (localStorage.getItem('theme') === 'dark') {
         light.style.display = 'none'
         dark.style.display = 'block'
         html.setAttribute('data-theme', 'dark')
+    } else if (!localStorage.getItem('theme')) {
+        dark.style.display = 'none'
     }
 
     // 主题切换
     light.addEventListener('click', () => {
-        html.setAttribute('data-theme', 'dark')
         light.style.display = 'none'
         dark.style.display = 'block'
+        html.setAttribute('data-theme', 'dark')
         localStorage.setItem('theme', 'dark');
     })
 
     dark.addEventListener('click', () => {
-        html.removeAttribute('data-theme')
         dark.style.display = 'none'
         light.style.display = 'block'
+        html.removeAttribute('data-theme')
         localStorage.removeItem('theme');
     })
 }
